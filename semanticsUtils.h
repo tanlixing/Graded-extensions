87ad4b2891fe15329c0c433a45bb1539b8d8fd08
tanlixing <tanlixing@126.com>
2022-07-20 09:59:08 +0800

    Create semanticsUtils.h

---
+++ semanticsUtils.h
@@ -0,0 +1,13 @@
+#ifndef SEMANTICSUTILS_H_INCLUDED
+#define SEMANTICSUTILS_H_INCLUDED
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <map>
+using namespace std;
+
+extern bool compare_extension(vector<int> vecExtSrc, vector<int> vecExtDes);
+
+#endif // SEMANTICSUTILS_H_INCLUDED
